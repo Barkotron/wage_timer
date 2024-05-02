d5e6bb3cb21cae3c22296c757528e6d22e42476e
Jackson Barker <jcrb45@hotmail.com>
2024-05-02 10:37:00 -0500

    added items that could be purchased

--- wage_timer/src/components/Results.jsx
+++ wage_timer/src/components/Results.jsx
@@ -1,12 +1,19 @@
-import { currencyFormatter } from "../util/formatting";
+import { currencyFormatter, toTwoDecimals } from "../util/formatting";
+import items from "../data/items";
+
 export default function Results({ elapsedTime, wage }) {
   function resultItem() {
     const amount = (wage/60/60/1000) * (elapsedTime);
+
+    const index = 8;
 
     return (
       <>
         <p>You've been working hard! you have made:</p>
         <p>{currencyFormatter.format(amount)}</p>
+
+        <p>That could get you: </p>
+        <p>{toTwoDecimals(amount/items[index].price)} {items[index].name}</p>
       </>
     );
   }
--- wage_timer/src/util/formatting.js
+++ wage_timer/src/util/formatting.js
@@ -2,3 +2,7 @@ export const currencyFormatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
   });
+
+  export const toTwoDecimals = (num) => {
+    return (Math.round(num * 10 ** 2) / 10 ** 2);
+}
