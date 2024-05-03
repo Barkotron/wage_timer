318d1fa14df9db82fc8c46b6bcdf46534299bd46
Jackson Barker <jcrb45@hotmail.com>
2024-05-03 13:47:15 -0500

    added steps to number inputs
    allows for working 1.5 hours and having a non-integer wage (ex 15.25/h)

--- wage_timer/src/components/WageInput.jsx
+++ wage_timer/src/components/WageInput.jsx
@@ -5,12 +5,12 @@ export default function WageInput({ formSubmit }) {
       <form onSubmit={formSubmit} id="wage-input-form">
         <div className="form-field">
           <label htmlFor="wage">Wage/h: </label>
-          <input type="number" id="wage" name="wage" required></input>
+          <input type="number" id="wage" name="wage" required step="0.05" min="0"></input>
         </div>
 
         <div className="form-field">
           <label htmlFor="time">Hours: </label>
-          <input type="number" id="time" name="time" required></input>
+          <input type="number" id="time" name="time" required step="0.5" min="0" ></input>
         </div>
 
         <div>
